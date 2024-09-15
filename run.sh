9c37380884d4233f984569718197145dcefef287
bhu1-103 <103.bhuvan@gmail.com>
2024-09-15 23:51:41 +0530

    update at Sun Sep 15 11:51:41 PM IST 2024

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/zsh
 
-cd step1; nvim input-java.csv; ./step1.sh; cd ../
+alias red='echo -e -n "\033[38;2;255;0;0m"'
+alias green='echo -e -n "\033[38;2;0;255;0m"'
+alias blue='echo -e -n "\033[38;2;0;0;255m"'
+
+cd step1; nvim input-java.csv; ./step1.sh; ;cd ../
 cd step2; ./step2.sh; ./points.sh|pbcopy; ./0.4/pls-work $(pbpaste); xdotool key super+f; cd ../
 cd step3; ./combine.sh; cd ../
 cd step4; ./saigo-no-steppu.sh;
