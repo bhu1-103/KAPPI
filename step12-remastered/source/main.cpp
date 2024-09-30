ce565230e3e7cb047623e1e5834f1ba094908ff8
bhu1-103 <103.bhuvan@gmail.com>
2024-09-30 23:59:31 +0530

    random number+

--- step12-remastered/source/main.cpp
+++ step12-remastered/source/main.cpp
@@ -6,8 +6,10 @@ int main()
 {
 	random_device rd;
 	mt19937 gen(rd());
-	uniform_int_distribution<> dis(0,99);
-	int nombor = dis(gen);
-	cout << "Uh... Here ya go: " << nombor << endl;
+	uniform_int_distribution<> dis(0,6);
+	int nombor_x = dis(gen);
+	int nombor_y = dis(gen);
+	cout << "here's x coordinate " << nombor_x*10 << endl;
+	cout << "here's y coordinate " << nombor_y*10 << endl;
 	return 0;
 }
