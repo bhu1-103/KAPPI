eb26d0720c4c190e245576a36bcf99ab786ec458
bhu1-103 <103.bhuvan@gmail.com>
2024-10-01 09:11:57 +0530

    circular coordinates rng

--- step12-remastered/source/main.cpp
+++ step12-remastered/source/main.cpp
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#define PI 3.141592653589793
+
+void co_ordinates(){
+	srand(time(0));	
+	double radius = 9.0+((double)rand()/RAND_MAX)*2.0;
+	double angle  = ((double)rand()/RAND_MAX)*2*PI;
+	printf("here ya go, %.2f\n", radius);
+	printf("here ya go, %.2f\n", angle);
+	printf("x and y cp-ordinates are (%.2f, %.2f)\n", radius*sin(angle), radius*cos(angle));
+}
 
 int main()
 {
-	srand(time(0));
-	int nombor = rand() % 100;
-	printf("here ya go, %d\n", nombor);
+	co_ordinates();
 	return 0;
 }
+
