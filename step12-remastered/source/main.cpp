11a330e9d21bb102e743a2a0ace530c336a1370c
bhu1-103 <103.bhuvan@gmail.com>
2024-10-01 09:30:38 +0530

    rng perfect+

--- step12-remastered/source/main.cpp
+++ step12-remastered/source/main.cpp
@@ -4,18 +4,17 @@
 #include <math.h>
 #define PI 3.141592653589793
 
-void co_ordinates(double r_low, double width){
+void co_ordinates(double r_low, double width, double z){
 	double radius = r_low+((double)rand()/RAND_MAX)*width;
 	double angle  = ((double)rand()/RAND_MAX)*2*PI;
-	printf("x and y cp-ordinates are (%.2f, %.2f)\n", radius*sin(angle), radius*cos(angle));
+	printf("1 A %.2f %.2f %.2f ", radius*sin(angle), radius*cos(angle), z);
 }
 
 int main()
 {
 	srand(time(0));	
-	co_ordinates(28.43,2.0);
-	co_ordinates(28.43,2.0);
-	co_ordinates(28.43,2.0);
+	for(int i=0;i<21;i++) co_ordinates(28.43,2.0,1.0);
+	printf("\n");
 	return 0;
 }
 
